45383190b3790d113461182ad05962cfd5001d1e
jose carlos goncalves <josecarlosgoncalves@outlook.pt>
2025-09-10 17:02:28 +0100

    01.basic

---
+++ 01.basic.js
@@ -0,0 +1,8 @@
+// console.log
+console.log("Hello, World!");
+console.log(42);
+console.log(true);
+console.log(null);
+console.log(undefined);
+console.table({ name: "Alice", age: 30 });
+console.log([1, 2, 3, 4, 5]);
